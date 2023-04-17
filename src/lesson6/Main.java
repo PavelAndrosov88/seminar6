6e614e632815b6645cea984735b7bfbd54eec1b3
PCHOME <andros88pa@mail.ru>
2023-04-17 19:55:10 +0700

    Lesson6

---
+++ src/lesson6/Main.java
@@ -0,0 +1,19 @@
+package lesson6;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Notebook> set = new HashSet<>();
+        set.add(new Notebook("Notebook 1", 8, "Windows8", 80000, "HP"));
+        set.add(new Notebook("Notebook 2", 16, "Windows10", 85000, "Asus"));
+        set.add(new Notebook("Notebook 3", 32, "linux", 85000, "Samsung"));
+        set.add(new Notebook("Notebook 4", 64, "MACOS", 90000, "MSI"));
+
+        OperationWithNotebook operation = new OperationWithNotebook(set);
+        operation.start();
+
+    }
+}
